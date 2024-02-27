580bdf7c468518b27d7a6adaa8ff2ed973d040ac
James Florez <james_florez@insideranken.org>
2024-02-27 17:10:07 -0600

    push

--- HOT2/MVCHOT2/obj/Debug/net8.0/MVCHOT2.AssemblyInfo.cs
+++ HOT2/MVCHOT2/obj/Debug/net8.0/MVCHOT2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCHOT2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b6c9a409db7f6f40fcf78345bb6927a5f44965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b73523adcc17cafac1184f222bcfb24a5a8df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCHOT2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCHOT2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
